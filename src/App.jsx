8414830797fd87b21ededf552ec131824ae47137
Rajiv0407 <kumarrajiv488@gmail.com>
2026-07-11 13:32:10 +0530

    Counter add and remove project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
+
 function App() {
+  const [counter,setCounter]=useState(0);
+
+  const addValue=()=>{
+    //console.log(counter);
+    if(counter <= 20){
+      setCounter(counter+1);
+    }
+  }
+  const removeValue=()=>{
+    if(counter > 0){
+    setCounter(counter-1);
+    }
+  }
+
+  
   return (
     <>
       <h1>Hello React | Rajiv kumar</h1>
+      <h2>Conter : {counter ? counter:0}</h2>
+      <br />
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
 
           </>
   )
 }
-
 export default App
